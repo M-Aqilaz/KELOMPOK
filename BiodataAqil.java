6b41ae677d547e70bc7808991f4a46e5f7ee0801
M-Aqilaz <muhaaqil6002@gmail.com>
2024-09-05 12:33:58 +0700

    Update BiodataAqil.java

--- BiodataAqil.java
+++ BiodataAqil.java
@@ -1,4 +1,4 @@
-public class Tugas17 {
+public class BiodataAqil {
     public static void main(String[] args) {
 
         System.out.println("---------------------------------------------------------");
