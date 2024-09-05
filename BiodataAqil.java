730ea2dd896a09674ebf54c869840920fe9391f9
M-Aqilaz <muhaaqil6002@gmail.com>
2024-09-05 11:59:23 +0700

    commit biodata Aqil

---
+++ BiodataAqil.java
@@ -0,0 +1,16 @@
+public class Tugas17 {
+    public static void main(String[] args) {
+
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama      : Muhammad Aqil Azami");
+        System.out.println("NIM       : 244107020128");
+        System.out.println("Kelas     : 1D");
+        System.out.println("No. Absen : 17");
+        System.out.println("Prodi     : D-IV Teknologi Informasi");
+        System.out.println("Alamat    : Pakisaji Kab. Malang");
+        System.out.println("No.HP     : 087858164187");
+        System.out.println("Hobi      : Membaca");
+    }
+}
