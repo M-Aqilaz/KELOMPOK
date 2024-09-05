f68c26f3c902cf90def4430153e584bfdd1f1d0c
FALAA-1285 <ahmadfalahi1285@gmail.com>
2024-09-05 12:10:21 +0700

    Commit biodata Falah

---
+++ Latihan03.java
@@ -0,0 +1,15 @@
+public class Latihan03 {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi Polinema--");
+        System.out.println("--------------------------------------------------");
+        System.out.println("Nama    :AHMAD FALAHI");
+        System.out.println("NIM     :244107020152");
+        System.out.println("Kelas   :1D");
+        System.out.println("No Absen :03");
+        System.out.println("Alamat  :Ds.pucuk, Kec.Pucuk, Kab.Lamongan");
+        System.out.println("No HP   :089652609705");
+        System.out.println("Hobi    :Futsal");
+    }
+    
+}
